e6ea091ff8ba961654c056851b25327368f57dde
Aluno (local) <Bir>
2016-08-30 21:42:20 -0300

    Ceaser pronto, Substituicao Pronto.

--- Cifras/Ceasar/ceaser.py
+++ Cifras/Ceasar/ceaser.py
@@ -3,13 +3,21 @@ arq = open("/home/aluno-local/Documentos/UFFS_Lucas/Seguranca/Cifras/inputs/1.in
 
 k = 17
 
-out = open("1.output", "wb")
-
+a = input()
 l = []
 
-for x in arq:
-	d = (x+k) %256
-	l.append(d)
-
-out.write(bytes(l))
-out.close()
+if a == 'e':
+	out = open("1_e.output", "wb")
+	for x in arq:
+		d = (x+k) % 256
+		l.append(d)
+	out.write(bytes(l))
+	out.close()
+if a == 'd':
+	out = open("1_e.output", "rb").read()
+	out2 = open("1_d.output", "wb")
+	for x in out:
+		d = (x-k) % 256
+		l.append(d)
+	out2.write(bytes(l))
+	out2.close()
